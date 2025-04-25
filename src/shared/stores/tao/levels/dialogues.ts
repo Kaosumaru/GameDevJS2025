d1105cfb1f9999a916e5dc32a06c7a9e7dfeb682
Adrianuse <s11036@pjwstk.edu.pl>
2025-04-25 13:13:39 +0200

    Start dialogue - first version

--- src/shared/stores/tao/levels/dialogues.ts
+++ src/shared/stores/tao/levels/dialogues.ts
@@ -1,7 +1,7 @@
 import { Dialogue, DialogueEntry } from '../dialogue';
 
 export const levelStart: Dialogue = {
-  entries: [e('goth-gf', 'Hi!'), e('knight', '𓀃𓀄𓀅𓀆!'), e('sun-princess', 'Hi!')],
+  entries: [e('goth-gf', 'Thanks again for saving us from our previous hero.'), e('sun-princess', 'It was truly unfortunate how he turned into a void beast.'), e('goth-gf', 'I thought it was kinda sexy.'), e('sun-princess', '...I think you’re kinda disturbed.'), e('knight', '𓀃𓀄𓀅𓀆!'), e('knight', '...'), e('goth-gf', 'Flattery won’t get you anywhere, stop your useless chatter and start fighting monsters.'), e('sun-princess', 'Tutorial_Basic'), e('sun-princess', 'Tutorial_Light_and_Dark')],
 };
 
 export const levelVictory: Dialogue = {
